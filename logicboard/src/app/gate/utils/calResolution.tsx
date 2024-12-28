93ed4f28637dca6865138690bceafad0dc057b9a
prasannashrestha011 <shresthaprashanna27@gmail.com>
2024-12-28 18:54:58 +0545

    added

--- logicboard/src/app/gate/utils/calResolution.tsx
+++ logicboard/src/app/gate/utils/calResolution.tsx
@@ -1,7 +1,7 @@
 import { Res } from "../types";
 
 export function calDeviceResolution():Res{
-    const width=window.innerWidth  
-    const height=window.innerHeight  
+    const width=window.innerWidth  *0.89
+    const height=window.innerHeight  * 0.89
     return {width,height}
 }
